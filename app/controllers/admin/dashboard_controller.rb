3db4864503008adf6a1f9602eb05a7bf1c34e9bd
Alvintol <alvin.tol@hotmail.com>
2022-05-04 22:03:45 +0000

    adds css for admin table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
   def show
     @category_total = Category.count
     @products_total = Product.count
