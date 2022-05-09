dfa3d87ffc863487dd9e4fa76f0d7fa777d28580
Alvintol <alvin.tol@hotmail.com>
2022-05-09 20:35:22 +0000

    corrects Rspec tests to include error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,26 +9,36 @@ RSpec.describe Product, type: :model do
       description: "test",
       image: 'plante_1.jpg',
       quantity: 0,
-      price: 64.99
-      })
-    end
-
+      price_cents: 64.99
+      })    
+  end
+  
   describe 'Validations' do
     it 'returns true Product includes name, price, quantity & category' do
-      expect(@product.name).to be_truthy
-      expect(@product.price).to be_truthy
-      expect(@product.quantity).to be_truthy
-      expect(@product.category).to be_truthy
+      expect(@product.valid?).to be_truthy
     end
-    it 'returns true Product includes name' do
-      expect(@product.name).to be_truthy
+
+    it 'returns nil when Product does not include name' do
+      @product.name = nil
+      @product.save
+      expect(@product.name).to be(nil)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
-    it 'returns true Product includes price' do
-      expect(@product.price).to be_truthy
+
+    it 'returns nil when Product does not include price' do
+      @product.price_cents = nil
+      @product.save
+      expect(@product.price_cents).to be(nil)
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
-    it 'returns true Product includes quantity' do
-      expect(@product.quantity).to be_truthy
+
+    it 'returns nil when Product does not include quantity' do
+      @product.quantity = nil
+      @product.save
+      expect(@product.quantity).to be(nil)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
+    
     it 'returns true Product includes category' do
       expect(@product.category).to be_truthy
     end
