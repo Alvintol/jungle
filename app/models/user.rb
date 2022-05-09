618c28a834e758becb2288d5e3e66eb481fc381a
Alvintol <alvin.tol@hotmail.com>
2022-05-09 22:50:02 +0000

    adds tests for user logins and validates unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  
   has_secure_password
+  
+  validates :first, :last, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
+  # validates :last, presence: true
+  # validates :email, presence: true 
+  # validates :password, presence: true
+  # validates :password_confirmation, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  before(:each) do
+    @user = User.new({
+      first: 'Bob',
+      last: 'Saget',
+      email: 'bob.saget@hotmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    })
+  end
+
+  describe 'Users' do
+    it 'returns true if created User is valid' do
+      expect(@user.valid?).to be_truthy
+    end
+
+    it 'returns false if created User is not valid' do
+      @user.email = nil
+      @user.password = 'test'
+      @user.save
+      expect(@user.valid?).to be_falsey
+    end
+
+    it 'returns false if email is not unique' do
+    @user2 = User.new({
+      first: 'Bob',
+      last: 'Saget',
+      email: 'bob.saget@hotmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    })
+    @user.save
+    @user2.save
+    expect(@user2.errors.full_messages).to include "Email has already been taken"
+    expect(@user2.valid?).to be_falsey
+    end
+
+    it 'returns false if email is not unique, also not case-sensitive' do
+    @user2 = User.new({
+      first: 'Bob',
+      last: 'Saget',
+      email: 'BOB.SAGET@hotmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    })
+    @user.save
+    @user2.save
+    expect(@user2.errors.full_messages).to include "Email has already been taken"
+    expect(@user2.valid?).to be_falsey
+    end
+
+    it 'returns true if email is unique' do
+    @user2 = User.new({
+      first: 'Finn',
+      last: 'The Human',
+      email: 'Adventure.time@hotmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }) 
+    @user.save
+    @user2.save
+    expect(@user2.valid?).to be_truthy
+    end
+  end
+end
+
+# :first, :last, :email, :password, :password_confirmation
+# Define validation specs
+# Password minimum length
+# New authentication (class) method
