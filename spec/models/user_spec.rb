106daf334e6a48e244943f3961b09b8e20d90685
Alvintol <alvin.tol@hotmail.com>
2022-05-10 01:52:28 +0000

    adds Rspec test to cover password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,76 +17,83 @@ RSpec.describe User, type: :model do
       expect(@user.valid?).to be_truthy
     end
 
+    
     it 'returns false if created User is not valid' do
       @user.email = nil
       @user.password = 'test'
       @user.save
       expect(@user.valid?).to be_falsey
     end
-
-    it 'returns false if email is not unique' do
-    @user2 = User.create({
-      first: 'Bob',
-      last: 'Saget',
-      email: 'bob.saget@hotmail.com',
-      password: 'password',
-      password_confirmation: 'password'
-    })
-    expect(@user2.errors.full_messages).to include "Email has already been taken"
-    end
-
-    it 'returns true if email is unique' do
-    @user2 = User.create({
-      first: 'Finn',
-      last: 'The Human',
-      email: 'Adventure.time@hotmail.com',
-      password: 'password',
-      password_confirmation: 'password'
-    }) 
-
-    expect(@user2.valid?).to be_truthy
-    end
     
-    it 'returns false if first name is nil' do
-    @user2 = User.create({
-      first: nil,
-      last: 'The Human',
-      email: 'Adventure.time@hotmail.com',
-      password: 'password',
-      password_confirmation: 'password'
+    it 'returns false if email is not unique' do
+      @user2 = User.create({
+        first: 'Bob',
+        last: 'Saget',
+        email: 'bob.saget@hotmail.com',
+        password: 'password',
+        password_confirmation: 'password'
+        })
+        expect(@user2.errors.full_messages).to include "Email has already been taken"
+      end
+      
+      it 'returns true if email is unique' do
+        @user2 = User.create({
+          first: 'Finn',
+          last: 'The Human',
+          email: 'Adventure.time@hotmail.com',
+          password: 'password',
+          password_confirmation: 'password'
+          }) 
+          
+          expect(@user2.valid?).to be_truthy
+        end
+        
+        it 'returns false if first name is nil' do
+          @user2 = User.create({
+            first: nil,
+            last: 'The Human',
+            email: 'Adventure.time@hotmail.com',
+            password: 'password',
+            password_confirmation: 'password'
     }) 
     @user2.save
     expect(@user2.errors.full_messages).to include "First can't be blank"
-    end
-
-    it 'returns false if last name is nil' do
+  end
+  
+  it 'returns false if last name is nil' do
     @user2 = User.create({
       first: 'Finn',
       last: nil,
       email: 'Adventure.time@hotmail.com',
       password: 'password',
       password_confirmation: 'password'
-    }) 
-    expect(@user2.errors.full_messages).to include "Last can't be blank"
+      }) 
+      expect(@user2.errors.full_messages).to include "Last can't be blank"
     end
-
+    
     it 'returns false if email is nil' do
-    @user2 = User.create({
-      first: 'Finn',
-      last: 'The Human',
-      email: nil,
-      password: 'password',
-      password_confirmation: 'password'
-    })
-    expect(@user2.errors.full_messages).to include "Email can't be blank"
-    end
+      @user2 = User.create({
+        first: 'Finn',
+        last: 'The Human',
+        email: nil,
+        password: 'password',
+        password_confirmation: 'password'
+        })
+        expect(@user2.errors.full_messages).to include "Email can't be blank"
+      end
 
-    it 'returns true if password length is at least 8 characters' do
-    @user2 = User.create({
-      first: 'Finn',
-      last: 'The Human',
-      email: 'Adventure.time@hotmail.com',
-      password: 'password',
+      it 'returns false if password is not confirmed' do
+        @user.password = 'wrongPassword'
+        @user.save
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
+      
+      it 'returns true if password length is at least 8 characters' do
+        @user2 = User.create({
+          first: 'Finn',
+          last: 'The Human',
+          email: 'Adventure.time@hotmail.com',
+          password: 'password',
       password_confirmation: 'password'
     }) 
     expect(@user2.password.length).to eql(8)
